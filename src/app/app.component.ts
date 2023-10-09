e9f6c3406084912a9a7730d3893817e929646007
Mihail Zafirovski <mihail.zafirovski@endava.com>
2023-10-09 21:52:12 +0200

    Redesign

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'doc-tales-fe';
+  showFooter = true;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.router.url);
+    this.showFooter = this.router.url !== '/';
+  }
 }
